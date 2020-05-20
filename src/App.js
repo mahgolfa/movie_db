1f05e704c92f176d75980e3177a0d5c87907fc46
mahgolfathi <m_fathi@arsh.co>
2020-05-21 01:42:21 +0430

    added axios and tested API

--- src/App.js
+++ src/App.js
@@ -2,25 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const axios = require('axios').default;
+
+class App extends React.Component {
+    state = {
+        movie: undefined
+    };
+
+    componentDidMount() {
+        this.getMoviesList('a')
+    }
+
+    getMoviesList(searchText) {
+        axios.get('https://api.themoviedb.org/3/search/movie?api_key=58f5191ea5d531ac90bb1fcc2d1119de&query=' + searchText)
+            .then( (response) => {
+                this.setState({movie: response.data.results[0]});
+                console.log(this.state.movie);
+            })
+            .catch( (error) => {
+                console.log(error);
+            })
+            .finally( () =>{
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
